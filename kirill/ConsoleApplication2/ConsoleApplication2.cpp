29177d0243b3318a71ea7071e45d56f69811a553
KirMishArt <159540936+KirMishArt@users.noreply.github.com>
2024-04-12 18:59:51 +0300

    Add files via upload

---
+++ kirill/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <omp.h>
+
+// Функция, производную которой нужно найти
+double f(double x) {
+    return std::sin(x);
+}
+
+// Функция для вычисления второй производной
+std::vector<double> secondDerivative(const std::vector<double>& xValues, double h) {
+    int size = xValues.size();
+    std::vector<double> result(size, 0.0);
+
+    // Вычисление второй производной с использованием параллельного программирования OpenMP
+#pragma omp parallel for
+    for (int i = 1; i < size - 1; ++i) {
+        result[i] = (f(xValues[i + 1]) - 2 * f(xValues[i]) + f(xValues[i - 1])) / (h * h);
+    }
+
+    return result;
+}
+
+int main() {
+    double a = 0.0;
+    double b = 10.0;
+    int n = 1000;
+    double h = (b - a) / n;
+
+    std::vector<double> xValues(n + 1);
+    for (int i = 0; i <= n; ++i) {
+        xValues[i] = a + i * h;
+    }
+
+    std::vector<double> d2f = secondDerivative(xValues, h);
+
+    // Вывод результатов
+    for (int i = 0; i < d2f.size(); ++i) {
+        std::cout << "x = " << xValues[i] << ", f''(x) = " << d2f[i] << std::endl;
+    }
+
+    return 0;
+}
---
+++ misha/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+void parallelShakerSort(vector<int>& arr) {
+    int left = 0;
+    int right = arr.size() - 1;
+    bool swapped = true;
+
+    while (swapped) {
+        swapped = false;
+
+#pragma omp parallel default(none) shared(arr, left, right, swapped)
+        {
+            int localLeft = left;
+            int localRight = right;
+
+#pragma omp for schedule(static)
+            for (int i = left; i < right; ++i) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr[i], arr[i + 1]);
+                    swapped = true;
+                    localRight = i;
+                }
+            }
+
+#pragma omp for schedule(static)
+            for (int i = right; i > left; --i) {
+                if (arr[i - 1] > arr[i]) {
+                    swap(arr[i - 1], arr[i]);
+                    swapped = true;
+                    localLeft = i;
+                }
+            }
+
+#pragma omp single
+            {
+                if (localLeft < localRight) {
+                    left = localLeft;
+                    right = localRight;
+                }
+                else {
+                    left = 0;
+                    right = arr.size() - 1;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = { 5, 8, 3, 1, 6, 2, 9, 7, 4 };
+
+    parallelShakerSort(arr);
+
+    for (size_t i = 0; i < arr.size(); ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
